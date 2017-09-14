9e34df26677490d0ece95614fb7507c2ad179991
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-09-14 10:47:11 +0300

    Kommentaar nt lisatud on esimene rida php koodi

---
+++ esimene.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Marii Helena Keerig </title>
+</head>
+<body>
+	<h1>Marii Helena Keerig, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	?>
+</body>
+</html>
