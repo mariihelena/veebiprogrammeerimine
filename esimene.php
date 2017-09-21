4b1f39ee3676db423ffd257d5806544120525fc3
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-09-21 10:23:52 +0300

    lisatud massiiv, for tsükkel ja esimene vorm

--- esimene.php
+++ esimene.php
@@ -2,6 +2,10 @@
 	//muutujad
 	$myName = "Marii Helena";
 	$myFamilyName = "Keerig";
+	$monthNamesEt = ["jaanuar", "veebruar", "m�rts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	//var_dump($monthNamesEt);  
+	//echo $monthNamesEt[8];
+	$monthNow = $monthNamesEt [date("n")-1];
 	
 	//hindan päeva osa
 	$hourNow = date("H");
@@ -15,7 +19,29 @@
 	if ($hourNow > 16) {
 		$partOfDay = "vaba aeg";
 	}
-	echo $partOfDay 
+	//echo $partOfDay
+	
+	//vanusega tegelemine
+	//var_dump($_POST);
+	//echo $_POST["birthYear"];
+	$myBirthYear;
+	$ageNotice = "";
+	if (isset($_POST["birthYear"]) and $_POST["birthYear"] != 0){
+		$myBirthYear = $_POST["birthYear"];
+		$myAge = date("Y")-$_POST["birthYear"];
+		$ageNotice = "<p>Te olete umbkaudu " .$myAge ." aastat vana.</p>";
+		
+		$ageNotice .= "<p> Olete elanud järgnevatel aastatel:</p> <ol>";
+		for ($i = $myBirthYear; $i <= date("Y"); $i ++){
+			$ageNotice .= "<li>" .$i ."</li>";
+		}
+		$ageNotice .= "</ol>";
+	}
+	
+	
+	/*for ($i = 0; $i < 5; $i ++){
+			echo "ha";
+	}*/
 ?>
 
 <!DOCTYPE html>
@@ -30,8 +56,19 @@
 	<?php
 		echo "<p>Algas PHP õppimine.</p>";
 		echo "<p>Täna on ";
-		echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date ("H:i:s");
-		echo ".</p>";
+		echo date("d. ") .$monthNow .date(" Y") .", kell oli lehe avamise hetkel " .date ("H:i:s");
+		echo ". Hetkel on " .$partOfDay ." </p>";
+	?>
+	<h2>Natuke vanusest</h2>
+	<form method="POST">
+		<label>Teie sünniaasta: </label>
+		<input name="birthYear" id="birthYear" type="number" value="<?php echo $myBirthYear; ?>" min="1900" max="2017">
+		<input name="submitBirthYear" type="submit" value="sisesta">
+	</form>
+	<?php
+		if ($ageNotice != ""){
+			echo $ageNotice;
+		}
 	?>
 </body>
 </html>
