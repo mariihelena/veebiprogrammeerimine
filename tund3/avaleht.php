bbb055de0a1cd4870666f32c45f43f6ff976db41
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-09-21 11:35:01 +0300

    Saab pilte suvalises järjekorras

---
+++ tund3/avaleht.php
@@ -0,0 +1,22 @@
+<?php
+	//muutujad
+	$myName = "Marii Helena";
+	$myFamilyName = "Keerig";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Marii Helena Keerig </title>
+</head>
+<body>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Meie õpime <a href="http://www.tlu.ee"> Tallinna Ülikoolis</a>.</p>
+	<p>Minu esimene php leht on <a href="../esimene.php">siin</a>.</p> 
+	<p>Minu sõbra Meelise leht on <a href="../../../~lutsmeel/Veebiprogrammeerimine">siin</a>.</p>
+	<p>Pilte näeb <a href="foto.php">siin</a>.</p>
+</body>
+</html>
