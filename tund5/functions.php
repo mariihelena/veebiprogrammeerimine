6b58cd84ffa6e637e11807f0fe14985e10a65aa6
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-10-19 00:21:43 +0300

    lisatud sisse- välja logimine

---
+++ tund5/functions.php
@@ -0,0 +1,81 @@
+<?php
+	$database = "if17_keermari";
+	
+	//alustame sessiooni
+	session_start();
+	
+	//sisselogimise funktsioon
+	function signIn($email, $password){
+		$notice = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, email, password FROM vp1users WHERE email = ?");
+		$stmt->bind_param("s",$email);
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb);
+		$stmt->execute();
+		
+		//kontrollime kasutajat
+		if($stmt->fetch()){
+			$hash = hash("sha512", $password);
+			if($hash == $passwordFromDb){
+				$notice = "Kõik korras! Logisimegi sisse!";
+				
+				//salvestame sessioonimuutujaid
+				$_SESSION["userId"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				//liigume pealehele
+				header("Location: main.php");
+				exit();
+			} else {
+				$notice = "Sisestasite vale salasõna!";
+			}
+		} else {
+			$notice = "Sellist kasutajat (" .$email .") ei ole!";
+		}
+		return $notice;
+	}
+	
+	//uue kasutaja andmebaasi lisamine
+	function signUp($signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword){
+		//loome andmebaasiühenduse
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		//valmistame ette käsu andmebaasiserverile
+		$stmt = $mysqli->prepare("INSERT INTO vp1users (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+		echo $mysqli->error;
+		//s - string
+		//i - integer
+		//d - decimal
+		$stmt->bind_param("sssiss", $signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword);
+		//$stmt->execute();
+		if ($stmt->execute()){
+			echo "\n Õnnestus!";
+		} else {
+			echo "\n Tekkis viga : " .$stmt->error;
+		}
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	//sisestuse kontrollimine
+	function test_input($data){
+		$data = trim($data);//eemaldab lõpust tühiku, tab vms
+		$data = stripslashes($data);//eemaldab "\"
+		$data = htmlspecialchars($data);//eemaldab keelatud märgid
+		return $data;
+	}
+	
+	/*$x = 8;
+	$y = 5;
+	echo "Esimene summa on: " .($x + $y);
+	addValues();
+	
+	function addValues(){
+		echo "Teine summa on: " .($GLOBALS["x"] + $GLOBALS["y"]);
+		$a = 4;
+		$b = 1;
+		echo "Kolmas summa on: " .($a + $b);
+	}
+	
+	echo "Neljas summa on: " .($a + $b);*/
+?>
